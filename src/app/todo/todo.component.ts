641db75698dc8d5ba92e421c14e0a560559d03e7
nihalmn028 <148523248+nihalmn028@users.noreply.github.com>
2024-03-19 19:17:57 +0530

    commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+todo1="understanding binding"
+enabled=true
+onClick(){
+  this.enabled=false
+  window.alert("You clicked on the button")
+}
+}
